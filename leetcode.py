2613e2584a739cc2c6bd592f8f801b1594ab5cfb
ABHISHEK VISHWAKARMA <73927352+ABHI-KARMA@users.noreply.github.com>
2022-08-19 19:42:46 +0530

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -31,3 +31,18 @@ class Solution(object):
                     return reference[:i]
             i += 1
         return reference[:m]
+    # https://leetcode.com/problems/string-to-integer-atoi/submissions/
+    def myAtoi(self, s):
+        if not len(s):
+            return 0
+        ls = list(s.strip())
+        if len(ls) == 0:
+            return 0
+        sign = -1 if ls[0] == '-' else 1
+        if ls[0] in ['-','+']:
+            del ls[0]
+        ret, i = 0, 0
+        while i < len(ls) and ls[i].isdigit():
+            ret = ret*10 + int(ls[i])
+            i += 1
+        return max(-2**31, min(sign * ret,2**31-1))
