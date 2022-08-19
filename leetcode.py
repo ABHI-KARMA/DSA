1fc2e6e59b80c7187c10efe386f6eabaa407a632
ABHISHEK VISHWAKARMA <73927352+ABHI-KARMA@users.noreply.github.com>
2022-08-19 18:29:34 +0530

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -2,6 +2,8 @@ class Solution(object):
     # https://leetcode.com/problems/roman-to-integer/
     def Romantointeger(self,str):
         self.st = str
+        if not len(self.str):
+            return -1
         dic = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         stack=[]
         res=0
