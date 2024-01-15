b3b3ca94d7ae21200012c0128e63871a6c3c120a
ABHISHEK VISHWAKARMA <73927352+ABHI-KARMA@users.noreply.github.com>
2024-01-15 19:42:11 +0300

    Update Leetcode-jan15.py

--- Leetcode-jan15.py
+++ Leetcode-jan15.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def countSymmetricIntegers(self, low, high):
+        # https://leetcode.com/problems/count-symmetric-integers/
         li = []
         for i in range(low, high+1):
             if len(str(i))%2 == 1:
@@ -13,3 +14,16 @@ class Solution(object):
             if sum(sm1) == sum(sm2):
                 li.append(i)
         return len(li)
+        
+    def sumBase(self, n, k):
+        # https://leetcode.com/problems/sum-of-digits-in-base-k/
+        res = ""
+        while n:
+            res =str(n%k) + res
+            n = n//k
+        sm = 0
+        res = int(res)
+        while res:
+            sm += res%10
+            res = res//10
+        return sm
